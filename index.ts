f026f878507e22d0cbeae98dc5124f6372957f98
Ray Zane <raymondzane@gmail.com>
2021-02-10 01:28:31 -0500

    This works quite well!

---
+++ index.ts
@@ -0,0 +1,95 @@
+/**
+ * In tooltips, intersections look a little ugly. This just flattens
+ * the intersections into one nice type.
+ */
+type Simplify<T> = { [K in keyof T]: T[K] };
+
+/**
+ * Parse a parameter. The following parameter formats are supported:
+ *   - :name - Required
+ *   - :name+ - Required
+ *   - :name? - Optional
+ *   - :name* - Optional
+ */
+type ParseParam<T extends string> = T extends `${infer Name}?`
+  ? { [key in Name]?: string | number }
+  : T extends `${infer Name}*`
+  ? { [key in Name]?: string | number }
+  : T extends `${infer Name}+`
+  ? { [key in Name]: string | number }
+  : { [key in T]: string | number };
+
+/**
+ * Chew through a string, looking for parameters.
+ */
+type Parse<T> = T extends `/:${infer Name}/${infer Next}`
+  ? Simplify<ParseParam<Name> & Parse<`/${Next}`>>
+  : T extends `/:${infer Name}`
+  ? ParseParam<Name>
+  : T extends `${infer _}${infer Next}`
+  ? Parse<Next>
+  : { [key: string]: never };
+
+/**
+ * Extract the URL parameters from a string.
+ */
+export type Params<URL extends string> = Parse<URL>;
+
+/**
+ * Constants
+ */
+const BLANK = "";
+const EMPTY_OBJECT = {};
+
+const RE_PARAMETER = /:(\w+)([\?\*]?)/g;
+const RE_SLASH_TRAILING = /\/$/;
+
+/**
+ * If the URL doesn't have any required parameters, the
+ * parameters argument should be optional.
+ */
+type OptionalArgs<T> = {} extends T ? [T] | [] : [T];
+type FormatArgs<T extends string> = OptionalArgs<Params<T>>;
+
+/**
+ * Format the URL
+ */
+export function format<URL extends string>(
+  url: URL,
+  ...args: FormatArgs<URL>
+): string {
+  const params = args[0] || EMPTY_OBJECT;
+
+  const resolve = (_match: string, name: string, optional: string) => {
+    const value = (params as any)[name];
+
+    if (value !== undefined) return value;
+    if (optional) return "";
+
+    throw new Error(`Parameter '${name}' is required in '${url}'`);
+  };
+
+  return url.replace(RE_PARAMETER, resolve).replace(RE_SLASH_TRAILING, BLANK);
+}
+
+/**
+ * Represents a mapping of URLs.
+ */
+interface Routes {
+  [key: string]: string;
+}
+
+/**
+ * Create a named route mapper.
+ */
+export function createNamedRoutes<R extends Routes>(routes: R) {
+  return <K extends keyof R>(name: K, ...args: FormatArgs<R[K]>): string => {
+    const route = routes[name];
+
+    if (route === undefined) {
+      throw new Error(`Route '${name}' is not defined`);
+    }
+
+    return format(route, ...args);
+  };
+}
---
+++ test.ts
@@ -0,0 +1,39 @@
+import { createNamedRoutes, format } from "./index";
+
+test("format", () => {
+  expect(format("/users")).toEqual("/users");
+
+  expect(format("/users/:id", { id: "9" })).toEqual("/users/9");
+  expect(format("/users/:id", { id: 9 })).toEqual("/users/9");
+
+  expect(format("/users/:id?")).toEqual("/users");
+  expect(format("/users/:id?", { id: "9" })).toEqual("/users/9");
+
+  expect(format("http://example.com/:id", { id: "1" })).toEqual(
+    "http://example.com/1"
+  );
+});
+
+test("format errors when a required parameter is missing", () => {
+  expect(() => format("/users/:id", {} as any)).toThrow();
+  expect(() => format("/users/:id+", {} as any)).toThrow();
+});
+
+test("format does not error when an optional parameter is missing", () => {
+  expect(() => format("/users/:id*", {} as any)).not.toThrow();
+  expect(() => format("/users/:id?", {} as any)).not.toThrow();
+});
+
+test("createNamedRoutes", () => {
+  const routes = { users: "/users", user: "/users/:id" } as const;
+  const route = createNamedRoutes(routes);
+
+  expect(route("users")).toEqual("/users");
+  expect(route("user", { id: "1" })).toEqual("/users/1");
+});
+
+test("createNamedRoutes errors when an unexpected route is given", () => {
+  const route = createNamedRoutes({} as any);
+
+  expect(() => route("users")).toThrow();
+});
